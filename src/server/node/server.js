f374744bb0d1dbbdb7bb0afbf7f8b4e24a638454
zach-planet-nine <zach@planetnine.app>
2024-09-19 23:23:49 -0700

    spellbook

--- src/server/node/server.js
+++ src/server/node/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import { createHash } from 'node:crypto';
 import { putUser, getUserByUUID, getUserByPublicKey, getNineum, deleteUser } from './src/routes/user.js';
 import { resolve } from './src/routes/magic.js';
 import { grant } from './src/routes/grant.js';
 import { transfer } from './src/routes/transfer.js';
+import db from './src/persistence/db.js';
 import nineum from './src/nineum/nineum.js';
 import experience from './src/experience/experience.js';
 import sessionless from 'sessionless-node';
+import bdo from 'bdo-js';
+import spellbook from './spellbooks/spellbook.js';
 
 const allowedTimeDifference = 300000; // keep this relaxed for now
 
@@ -27,6 +31,37 @@ app.use((req, res, next) => {
   console.log('\n\n', req.body, '\n\n');
   next();
 });
+
+const SUBDOMAIN = process.env.SUBDOMAIN || 'dev';
+bdo.baseURL = `${SUBDOMAIN}.bdo.allyabase.com`;
+
+const bdoHashInput = `${SUBDOMAIN}fount`;
+
+const bdoHash = createHash('sha256').update(bdoHashInput).digest('hex');
+
+const repeat = (func) => {
+  setTimeout(func, 2000);
+};
+
+const bootstrap = async () => {
+  try {
+    const bdoUUID = await bdo.createUser(bdoHash, spellbook, db.saveKeys, db.getKeys);
+    const fount = {
+      uuid: 'fount',
+      bdoUUID,
+      spellbook
+    };
+
+    if(!fount.bdoUUID) {
+      throw new Error('bootstrap failed');
+    }
+
+    await db.saveUser(fount);
+  } catch(err) {
+    repeat(bootstrap);
+  }
+};
+
 
 app.put('/user/create', putUser);
 app.get('/user/:uuid', getUserByUUID);
